e14d98256f8b8a16f4ed9d1700ef56a35ee1e8e2
Observer98 <wxz17141432@outlook.com>
2021-12-08 00:08:42 -0500

    Add shared memory method

--- testmulti.py
+++ testmulti.py
@@ -30,16 +30,71 @@ class multi:
             
             if ((row<M_h)&&(col<N_w)){
                 float temp = 0.0f;
-            
+                
                 for(int i=0; i<M_w;i++){
                     temp += M[i+row*M_w]*N[col+i*N_w];
                 }
-            
+                
                 P[row*M_w+col] += temp;
             }
         }
+        //---------naive ends-------------------------------------------
         
         
+        //---------shared starts----------------------------------------
+        #define blocksize 32  // which equals tile size and blockDim.x,y
+                                // should be the same in host code
+                                
+        __global__ void mul_shared(float *M, float *N, 
+                                  const int Mw, const int Mh, const int Nw, 
+                                  float *P){
+        // input M size = Mh*Mw
+        // input N size = Mw*Nw
+        // output P size = Mh*Nw
+            
+            __shared__ float MS[blocksize][blocksize];
+            __shared__ float NS[blocksize][blocksize];
+            
+            int tx = threadIdx.x;
+            int ty = threadIdx.y;
+            int bx = blockIdx.x;
+            int by = blockIdx.y;
+            
+            int row = ty + by*blocksize;
+            int col = tx + bx*blocksize;
+            
+            // int strideM = ceil(Mh/blocksize);
+            int strideM = Mw/blocksize +1;
+            
+            float temp = 0.0f;
+            for(int i=0; i<strideM;i++){
+                
+                if (i*blocksize+tx<Mw && row <Mh){
+                    MS[ty][tx] = M[row*Mw+i*blocksize+tx];
+                }
+                else{
+                    MS[ty][tx] = 0.0f;       //padding block elementsthat exceeds M's margin with 0
+                }
+                
+                if (i*blocksize+ty<Mw && col <Nw){
+                    NS[ty][tx] = N[(i*blocksize+ty)*Nw+col];
+                }
+                else{
+                    NS[ty][tx] = 0.0f;
+                }
+                __syncthreads();
+                
+                
+                for(int j=0;j<blocksize;j++){
+                    temp += MS[ty][j]*NS[j][tx];
+                }
+                __syncthreads();
+            }
+            if(row<Mh && col< Nw){
+                P[row*Nw+col] = temp;
+            }
+        }
+        //----------shared ends-----------------------------------------------
         """
         return SourceModule(kernelwrapper)
 
@@ -52,7 +107,6 @@ class multi:
         start = cuda.Event()
         end = cuda.Event()
         start.record()
-        P_size = M_h * N_w
         dM = cuda.mem_alloc_like(M)  # create input array in device
         dN = cuda.mem_alloc_like(N)
         dP = cuda.mem_alloc_like(P)
@@ -70,8 +124,26 @@ class multi:
 
         return P, start.time_till(end)
 
-    def multi_tile(self):
-        pass
+    def multi_tile(self, M, N, M_w, M_h, N_w):
+        P = np.zeros(shape=(M_h, N_w)).astype(np.float32)  # create output array in host
+        func = self.mod.get_function("mul_shared")
+        start = cuda.Event()
+        end = cuda.Event()
+        start.record()
+        dM = cuda.mem_alloc_like(M)  # create input array in device
+        dN = cuda.mem_alloc_like(N)
+        dP = cuda.mem_alloc_like(P)
+        cuda.memcpy_htod(dM, M)  # copy input array from host to device
+        cuda.memcpy_htod(dN, N)
+
+        # kernel call
+        griddimx = self.getgriddim(M_h, block)
+        griddimy = self.getgriddim(N_w, block)
+        func(dM, dN, M_w, M_h, N_w, dP, block=blocksize, grid=(griddimx, griddimy, 1))
+        cuda.memcpy_dtoh(P, dP)
+        end.record()
+        end.synchronize()
+        return P, start.time_till(end)
 
 
 def pytest(a, b):
@@ -87,27 +159,28 @@ blocksize = (block, block, 1)
 if __name__ == "__main__":
     cuda_model = multi()
 
-    M_h = 4096
-    M_w = 4096
-    N_w = 4096
+    M_h = 2048/2
+    M_w = 2048/2
+    N_w = 2048/2
     M_h = np.int32(M_h)
     M_w = np.int32(M_w)
     N_w = np.int32(N_w)
 
     M = np.random.randint(1, 5, (M_h, M_w)).astype(np.float32)
     N = np.random.randint(1, 5, (M_w, N_w)).astype(np.float32)
-
     P_naive,t_naive = cuda_model.multi_naive(M,N,M_w,M_h,N_w)
+    P_shared,t_shared = cuda_model.multi_tile(M,N,M_w,M_h,N_w)
     P_py,t_py = pytest(M, N)
-
     try:
         # print("Checkpoint: Do python and gpu convolution match? Checking...")
-        assert ((P_naive == P_py).all())
+        assert ((P_shared == P_py).all())
         print("match, result:")
     except AssertionError:
         print("conv results do not match ")
 
     print("python test = \n",P_py)
-    print("P_naive = \n",P_naive)
-    print("t_naive=\n",t_naive)
-    print("t_py=\n",t_py)
+    print("P_shared = \n",P_shared)
+
+    print("t_naive=\n", t2-t1)
+    print("t_shared=\n",t3-t2)
+    print("t_py=\n",t4-t3)
